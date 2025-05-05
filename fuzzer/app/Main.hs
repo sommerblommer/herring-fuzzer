e39016b541c585a2853432b54ca311630366d34b
au701790 <au701790@uni.au.dk>
2025-05-05 14:50:04 +0200

    flere gadgets

--- fuzzer/app/Main.hs
+++ fuzzer/app/Main.hs
@@ -1,10 +1,14 @@
 module Main (main) where
 
-import Control.Monad (replicateM)
+import Control.Monad (replicateM, foldM)
 import System.Process
 import System.Random
-import System.Random.Stateful (Uniform (uniformM))
-import Text.Read (Lexeme (String))
+import System.Random.Stateful (Uniform (uniformM), StatefulGen (uniformWord8))
+import Data.Bits
+
+
+data Env = Env {funs :: [(String, Int)]}
+
 
 main :: IO ()
 main = do
@@ -17,7 +21,7 @@ main = do
   let sam_path = "/home/samuel/lbs-projekt/Herring-lang/.stack-work/dist/x86_64-linux/ghc-9.4.8/build/herring-exe/herring-exe"
   let sam_pathToInput = "/home/samuel/lbs-projekt/herring-fuzzer/fuzzer/input.txt"
 
-  (exitCode, stdOut, stdErr) <- readProcessWithExitCode sam_path [sam_pathToInput] ""
+  (exitCode, stdOut, stdErr) <- readProcessWithExitCode alex_path [alex_pathToInput] ""
   putStrLn $
     "Exit code: "
       ++ show exitCode
@@ -26,20 +30,90 @@ main = do
       ++ "\nErr: "
       ++ stdErr
 
+
+emptyEnv :: Env 
+emptyEnv = Env {funs = []}
+
 mainGadget :: IO String
 mainGadget = do
-  exp1 <- expGadget
+  (fc, en) <- functionGadget emptyEnv 10 
+  exp1 <- expGadget en
   rStr <- strGadget
-  return $ "main : Int let " ++ rStr ++ " = print(" ++ exp1 ++ ") in return 0\n"
+  exp2 <- expGadget en
+  funcall <- funcallGadget en
+  op <- opGadget
+  return $ fc ++ " main : Int\n\tlet _ = print(" ++ exp1 ++ op ++ funcall ++ ")\n\tin return 0\n"
 
-expGadget :: IO String
-expGadget = numGadget
 
-numGadget :: IO String
-numGadget = do
-  r <- getStdRandom (randomR (0 :: Int, 100000 :: Int))
+
+functionGadget :: Env -> Int -> IO (String, Env) 
+functionGadget en argLen = do 
+    name <- validIdentGadget 
+    let argNames = [validIdentGadget | _ <- [0..argLen]]
+    let start = name ++ " : "
+    let args :: [IO String] -> IO String
+        args [] = return " Int"
+        args [x] = do 
+            arg <- x
+            return $ "( " ++ arg ++  " : Int) -> Int\n" 
+        args (x:xs) = do 
+            arg <- x
+            rest <- args xs
+            return $ "( " ++ arg ++ " : Int) -> " ++ rest
+    str <- args argNames
+    let nenv = en {funs = (name, argLen) : funs en} 
+    e <- expGadget nenv
+    return (start ++ str ++ "\treturn " ++ e ++ "\n", nenv)
+    
+
+
+
+expGadget :: Env -> IO String
+expGadget en = do 
+    lottery <- randomRIO (0 :: Int, 5)
+    putStrLn $ "first Lottery : " ++ show lottery
+    case lottery of 
+        _ -> numGadget 99999999
+
+funcallGadget :: Env -> IO String 
+funcallGadget en 
+    | null (funs en) = return "" 
+    | otherwise = do 
+        let len = length $ funs en 
+        putStrLn $ "amount of funs : " ++ show len
+        num <- randomRIO (0, len - 1) 
+        putStrLn $ "lottery : " ++ show  num
+        let (fname, argLen) = funs en !! num
+        let args :: Int -> IO String 
+            args 1 = expGadget en 
+            args n = do 
+                e <- expGadget en 
+                rest <- args (n - 1)  
+                return $ e ++ ", " ++ rest
+        a <- args (argLen + 1) 
+        return $ "(" ++ fname ++ "(" ++ a ++ "))"
+
+    
+    
+
+numGadget :: Int ->  IO String
+numGadget i = do
+  r <- randomRIO (0, i)
   return $ show r
+
+opGadget :: IO String 
+opGadget = do
+    a <- randomRIO (1 :: Int, 4 :: Int)
+    case a of 
+        1 -> return "+"
+        2 -> return "-"
+        3 -> return "*"
+        4 -> return "/"
+        _ -> return "+"
 
 strGadget :: IO String
 strGadget = do
   flip replicateM (randomRIO (' ', '~')) =<< randomRIO (1, 32)
+
+validIdentGadget :: IO String 
+validIdentGadget  = flip replicateM (randomRIO ('a', 'z')) =<< randomRIO (1, 32)
---
+++ fuzzer/output.ll
@@ -0,0 +1,37 @@
+
+declare void @print_integer(i32)
+declare i32 @read_integer()
+%struct.list = type { i32, [0 x i32] }
+define i32 @main() {
+	%call0 = call i32 @ubmwpqwoywhyqswjkrpizaf(i32 59635740, i32 13021238)
+	%bop1 = mul i32 50031963, %call0
+	call void @print_integer(i32 %bop1)
+	ret i32 0
+
+}
+define i32 @ubmwpqwoywhyqswjkrpizaf(i32 %zijifxlhtzcpkodprdctkjtyyjsr, i32 %sttfofspsgwakiw, i32 %usvmoku, i32 %op, i32 %vupsuadmhkinpvgqjara, i32 %ilshklsmynaogygbwoblrtstr, i32 %xpztnzxuncwhfvuahsj, i32 %jwbtkabjhxfsq, i32 %bixenhv, i32 %yljkrtgupofdkykvsintvpusp, i32%fxcaq) {
+	%fxcaqptr0 = alloca i32
+	%yljkrtgupofdkykvsintvpuspptr1 = alloca i32
+	%bixenhvptr2 = alloca i32
+	%jwbtkabjhxfsqptr3 = alloca i32
+	%xpztnzxuncwhfvuahsjptr4 = alloca i32
+	%ilshklsmynaogygbwoblrtstrptr5 = alloca i32
+	%vupsuadmhkinpvgqjaraptr6 = alloca i32
+	%opptr7 = alloca i32
+	%usvmokuptr8 = alloca i32
+	%sttfofspsgwakiwptr9 = alloca i32
+	%zijifxlhtzcpkodprdctkjtyyjsrptr10 = alloca i32
+	store i32 %fxcaq, ptr %fxcaqptr0
+	store i32 %yljkrtgupofdkykvsintvpusp, ptr %yljkrtgupofdkykvsintvpuspptr1
+	store i32 %bixenhv, ptr %bixenhvptr2
+	store i32 %jwbtkabjhxfsq, ptr %jwbtkabjhxfsqptr3
+	store i32 %xpztnzxuncwhfvuahsj, ptr %xpztnzxuncwhfvuahsjptr4
+	store i32 %ilshklsmynaogygbwoblrtstr, ptr %ilshklsmynaogygbwoblrtstrptr5
+	store i32 %vupsuadmhkinpvgqjara, ptr %vupsuadmhkinpvgqjaraptr6
+	store i32 %op, ptr %opptr7
+	store i32 %usvmoku, ptr %usvmokuptr8
+	store i32 %sttfofspsgwakiw, ptr %sttfofspsgwakiwptr9
+	store i32 %zijifxlhtzcpkodprdctkjtyyjsr, ptr %zijifxlhtzcpkodprdctkjtyyjsrptr10
+	ret i32 18950642
+
+}
